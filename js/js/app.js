7582e63134aca830fec0d118560b3cfb8bf5e9c4
nopparatkeawking-dev <nopparatkeawking@gmail.com>
2026-07-20 11:08:38 +0700

    Create app.js

---
+++ js/js/app.js
@@ -0,0 +1,9 @@
+const btn=document.getElementById("scanBtn");
+
+btn.onclick=()=>{
+
+document.getElementById("productName").innerHTML="กำลังวิเคราะห์...";
+
+document.getElementById("confidence").innerHTML="";
+
+};
